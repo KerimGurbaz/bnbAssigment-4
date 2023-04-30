d8cca88a18736abd42a1ba27371e7de24e8c4f95
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-30 23:00:21 +0200

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,12 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  solidity: "0.8.4",
   networks: {
     bscTestnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId:97,
       gasPrice:20000000000,
-      accounts:{mnemonic: mnemonic}
+
     }
   }
 };
+
